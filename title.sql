e1f09d17135c3aca4c877cd7ee55a7a89b7133ca
Tor Andersson <tor@ccxvii.net>
2022-01-01 01:38:24 +0100

    Split into separate module.

---
+++ title.sql
@@ -0,0 +1,3 @@
+insert or replace into titles ( title_id, title_name, bgg ) values ( 'shores-of-tripoli', 'Shores of Tripoli', 237860 );
+insert or replace into roles values ( 'shores-of-tripoli', 'Tripolitania' );
+insert or replace into roles values ( 'shores-of-tripoli', 'United States' );
